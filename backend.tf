4d10862e7019cbd5ee0674d3c1af988529e36fab
vrushaliajit <vrushalilambe2018@gmail.com>
2025-06-03 17:26:41 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,6 +1,9 @@
-backend "s3" {
-  bucket         = "mydev-project-terraform-sample-batch-267"
-  key            = "main"
-  region         = "ap-south-1"
-  dynamodb_table = "my-dynamodb-table"
+terraform {
+  backend "s3" {
+    bucket         = "mydev-project-terraform-sample-batch-267"
+    key            = "main"
+    region         = "ap-south-1" 
+    dynamodb_table = "my-dynamodb-table"
+    encrypt        = true
+  }
 }
